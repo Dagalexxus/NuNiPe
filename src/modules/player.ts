9ce46a9651ad5dd26786aa72e09b97f1575e2bd1
Peer <PeerVogel@gmx.de>
2022-06-06 17:13:16 +0200

    tried animation

--- src/modules/player.ts
+++ src/modules/player.ts
@@ -6,9 +6,9 @@ export class Player{
 
     constructor(towntexture:string){
         this.town=new Town(towntexture);
-        this.town.add_building(new Building(200,200,"house"));
-        this.town.add_building(new Building(150,250,"house"));
-        this.town.add_building(new Building(260,270,"house"));
+        this.town.add_building(new Building(200,200,"houseanimation"));
+        this.town.add_building(new Building(150,250,"houseanimation"));
+        this.town.add_building(new Building(260,270,"houseanimation"));
         this.town.add_building(new Building(370,300,"tree"));
         this.town.add_building(new Building(480,350,"house"));
         this.town.add_building(new Building(290,450,"tree"));
